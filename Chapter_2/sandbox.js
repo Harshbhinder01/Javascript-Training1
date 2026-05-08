72107f013991ad3b4e432d5b3d43ce14a8183c4a
Harsh Bhinder <hbhinder@rrc.ca>
2026-05-08 15:59:04 -0500

    28:33 second video

---
+++ Chapter_2/sandbox.js
@@ -0,0 +1,44 @@
+// let age = 25;
+// let year = 2026;
+
+// console.log(age, year);
+
+// // updates the age variable to 30
+// age = 30;
+
+// console.log(age);
+
+
+// const points = 100;
+
+// console.log(points);
+
+// var score = 75;
+// console.log(score)
+
+
+// strings
+console.log("hello world")
+
+let email = "harshbhinder@gmail.com"
+console.log(email)
+
+// string conactenation
+let firstName = "harshdeep"
+let lastName = "Bhinder"
+
+// string is used for space
+let fullName = firstName + "" + lastName
+
+console.log(fullName)
+
+// getting characters
+console.log(fullName[2]);
+
+// string length
+console.log(fullName.length)
+
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result)
