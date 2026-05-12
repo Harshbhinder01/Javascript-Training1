4f8a217fa22ad5d900c9c502b4fb9db9081ea017
Harshdeep <hbhinder@rrc.ca>
2026-05-12 10:44:11 -0500

    finished chaper 4

---
+++ Chapter_4/sandbox.js
@@ -0,0 +1,84 @@
+// functions
+
+// function declaration 
+
+// function greet() {
+//     console.log("hello there");
+// }
+
+// function expression (store a function in a variable)
+// const speak = function() {
+//     console.log("good day")
+// };
+
+// calling the function
+// greet();
+
+// speak();
+
+
+// arguments and paramaters
+// const speak = function(time = "night", name = "harsh") {
+//     console.log(`good ${morning} ${name}`);
+// }
+
+// speak();
+// speak("day", "james");
+
+
+// returning values
+// const calcArea = function(radius){
+//     return 3.14 * radius**2;
+    
+// };
+
+// const area = calcArea(5);
+
+// console.log(area)
+
+
+// const calcVol = function(area) {
+
+// }
+
+// calcVol(area)
+
+
+// arrow functions
+// if you get rid of the curly braces and the return it will automatically return it for you
+// const calcArea = (radius) => {
+//     return 3.14 * radius**2;
+// };
+
+// const area = calcArea(5);
+
+// console.log(area)
+
+// practice arrow functions
+
+// const greet = () => "hello world";
+
+// const result = greet();
+// console.log(result);
+
+
+// const bill = (product, tax) => {
+//     let total = 0;
+//     for(let i = 0; i < product.length; i++) {
+//         total += products[i] + products[i] * tax;
+//     }
+//     return total;
+// }
+
+// const name = "harsh"
+// // functions
+// const greet = () => "hello";
+
+// let resultOne = greet();
+// console.log(resultOne);
+
+// // methods
+
+// let resultTwo = name.toUpperCase();
+// console.log(resultTwo)
+
