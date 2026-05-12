cb2724a9e940697f5262ad941386c181cae76281
Harshdeep <hbhinder@rrc.ca>
2026-05-12 13:11:48 -0500

    finished chapter 5

---
+++ Chapter_5/sandbox.js
@@ -0,0 +1,99 @@
+//  object literals
+
+
+
+// let user = {
+//     name: "harshdeep",
+//     age: 30,
+//     email: "harshbhinder@gmail.com",
+//     location: "canaca",
+//     blogs:["why mac and cheese rules", "10 things to make with marmite"]
+// };
+
+// console.log(user);
+// console.log(user.name);
+
+// // change the age using dot notation
+// user.age = 25
+
+// const key = "location";
+// // change the name using square bracket notation
+// console.log(user["email"])
+// user["name"] = "chun-li";
+// console.log(user["name"])
+
+// // type of operators
+// console.log(typeof user);
+
+// arrays of objects
+// const blogs = [
+//     {title: "why mac and cheese rules", likes: 30},
+//     {title: "10 things to make with marmite", likes: 50}
+// ];
+
+// console.log(blogs)
+
+
+// let user = {
+//     name: "harshdeep",
+//     age: 30,
+//     email: "harshbhinder@gmail.com",
+//     location: "canaca",
+//     blogs:[
+//         {title: "why mac and cheese rules", likes: 30},
+//         {title: "10 things to make with marmite", likes: 50}
+//     ],
+//     login: function(){
+//         console.log("the user logged in")
+//     },
+//     logout: function(){
+//         console.log("the user logged out")
+//     },
+//     // this will log the blogs usint "this"
+//     logBlogs: () => {
+//         console.log(this.blogs);
+//         console.log("this user has written the following blogs:");
+//         this.blogs.forEach(blog => {
+//             console.log(blog.title, blog.likes);
+//         })
+//     }
+// };
+
+// user.logBlogs();
+// console.log(this);
+
+// // adding methods to objects
+// user.login();
+// user.logout();
+
+// const name = "harsh"
+// name.toUpperCase();
+
+
+// // math object
+
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// const area = 7.7;
+
+// // .round rounds numbers up
+// console.log(Math.round(area));
+// // rounds it down
+// console.log(Math.floor(area));
+// // rounds it up
+// console.log(Math.ceil(area));
+// // takes away the int (number after the .)
+// console.log(Math.trunc(area));
+
+// // random numbers
+// const random = Math.random();
+
+// console.log(random);
+// // random number between 1-100
+// console.log(Math.round(random * 100));
+
+
+
+
