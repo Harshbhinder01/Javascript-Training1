21bfe7fe0482c73b68582e0697fabc11d9be8e63
Harshdeep <hbhinder@rrc.ca>
2026-05-12 08:47:00 -0500

    finished video 3

---
+++ Chapter_3/sandbox.js
@@ -0,0 +1,82 @@
+//  for loops
+// for(let i = 0; i < 5; i++) {
+//     console.log('in loop:', i);
+// }
+
+// console.log("loop finished");
+
+// const names = ['shaun', 'mario', 'luigi' ]
+
+
+// for(let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+// while loops
+// const names = ["shaun", "mario", "luigi"];
+// let i = 0
+
+// while(i < 5) {
+//     console.log("in loop: ", i)
+//     i++;
+// }
+
+// let i = 0;
+
+// while (i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+// do while loops
+//  let i = 5;
+
+// do {
+//     console.log("value of i is: ", i);
+//     i++;
+// } while (i < 5;)
+
+
+// if statements
+
+// const age = 20
+
+// if (age > 20) {
+//     console.log("you are over 20 years old")
+// }
+
+// this is an example of if else statements 
+// let password = "harshdeep"
+
+// if (password.length >= 2) {
+//     console.log("that password is long enough")
+// } else {
+//     console.log("this password is no long enough")
+// }
+
+// else if statement 
+
+// const password = "coolbanana"
+
+// if(password.length >= 9) {
+//     console.log("this is a strong password")
+// } else if (password.length >=5) {
+//     console.log("this is a lone enough password")
+// } else {
+//     console.log("the password has to be 5 characters long")
+// }
+
+
+
+// // logical operators - OR || and AND &&
+// const password = "coolbanana1234"
+
+// if(password.length >= 9 && password.includes("@")) {
+//     console.log("this is a strong password")
+// } else if (password.length >= 5 || password.includes("@") && password.length > 5) {
+//     console.log("this is a lone enough password")
+// } else {
+//     console.log("the password has to be 5 characters long")
+// }
+
+
